05cd2bcb2131a2f462a8d0abc883a21a98c2e7ef
Duncan Hall <duncan.hall@students.olin.edu>
2017-09-28 16:34:38 -0400

    little stuff

--- networks.py
+++ networks.py
@@ -20,6 +20,7 @@ def gen_BA_graph(n, k):
     """ generate one node at a time, connecting preferentially to higher degree nodes """
     pass
 
+
 # predetirmined graph (this will be used to grab the facebook graph)
 def load_graph(filename):
     pass
--- simulation.py
+++ simulation.py
@@ -4,11 +4,12 @@ viruses/disease through connected networks
 
 Wrtie Down each individual steps very clearly before any implementation
 
-1.
-2.
-3.
-4.
-5.
-6.
+1. generate a network
+2. generate some percentage of initially infected nodes
+3. for some # of time steps, run simulation of concurrent infection/recovery
 
 """
+
+def run_SIS_simulation(graph, time_step=1000):
+    """ """
+    pass
