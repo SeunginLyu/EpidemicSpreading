ffbe45d651005dc202c63f4ccd70d01e1da31c1d
Duncan Hall <duncan.hall@students.olin.edu>
2017-09-30 20:33:23 -0400

    added basic timeseries plot and jup nb stuff

--- code/display.py
+++ code/display.py
@@ -2,7 +2,10 @@
 All plotting and animation functions for anylizing and visualizing the spread
 of disease.
 """
+import thinkplot
 
+def plot_all_time_steps(infection_data):
+    thinkplot.plot([0, len(infection_data)], [0.0, 1.0], color='gray')
 
 def plot():
     pass
